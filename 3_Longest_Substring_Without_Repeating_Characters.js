52443f0c57a70e1788f7847ba2ed149654fccd54
duzhaoyang <duzhaoyang@meituan.com>
2016-01-11 17:49:43 +0800

    update problem 3

--- 3_Longest_Substring_Without_Repeating_Characters.js
+++ 3_Longest_Substring_Without_Repeating_Characters.js
@@ -9,7 +9,7 @@
  * 状态的定义：F(n) 为以字符s[n]结尾且包含s[n]的最长不重复子串的长度。
  * 转移方程的定义：
  * F(n) = F(n-1) + 1, 当不存在k使得s[k]===s[n] && k < n
- * F(n) = F(n-1) + 1, k 满足max(s[k] === s[n]) && k < n && F(n-1) < n - k
+ * F(n) = F(n-1) + 1, k 满足s[max(k)] === s[n] && k < n && F(n-1) < n - k
  * F(n) = n - k, k 满足max(s[k] === s[n]) && k < n && F(n-1) > n - k
  *
  * 最终所求解为max(F(i)), 用数组保存每一步F(n)的值，用对象保存s[k]的最大重复index
@@ -27,8 +27,8 @@ var lengthOfLongestSubstring = function(s) {
     
     var exsists = {}, preMaxLen;
     var maxLen = 1;
-    var repeatIndex;
-    preMaxLen = 1;
+    var repeatIndex; // max(k)
+    preMaxLen = 1; // F[n-1]
     exsists[s[0]] = 0;
     for(var i=1; i<s.length; ++i){
         if(exsists[s[i]] !== undefined) {
