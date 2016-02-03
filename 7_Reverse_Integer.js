6f97ee7cc29c2d8c4aa068a9e31489cbd91a1225
duzhaoyang <duzhaoyang@meituan.com>
2016-02-03 19:23:16 +0800

    update problem 7

--- 7_Reverse_Integer.js
+++ 7_Reverse_Integer.js
@@ -33,7 +33,33 @@ var reverse = function(x) {
         res += arr[i] * Math.pow(10, i);
     }
     res = x >= 0 ? res : -res;
+
+    // 为了过OJ整数溢出判断
+    if (res > 2147483647 || res < -2147483647) {
+        res = 0;
+    }
     return res;
 };
 
-console.log(reverse(-23));
+// 下面这种方法显然比我高明得多，来自这里：https://leetcode.com/discuss/84344/share-my-answer
+var reverse1 = function(x) {
+    var res = 0;
+    var temp = x;
+    if (temp < 0) {
+        temp = -temp;
+    }
+    while (temp) {
+        res = res * 10 + temp % 10;
+        temp = Math.floor(temp / 10);
+    }
+    res = x < 0 ? -res : res;
+
+    // 为了过OJ整数溢出判断
+    if (res > 2147483647 || res < -2147483647) {
+        res = 0;
+    }
+
+    return res;
+};
+
+console.log(reverse1(-1233434));
